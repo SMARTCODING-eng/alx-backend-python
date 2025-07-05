9312454d578ec21fc60069da33df888c99e52aa0
Elutayo <elutayojohnson@gmail.com>
2025-07-05 18:52:41 +0100

    modify

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -14,7 +14,7 @@ def paginate_users(page_size, offset):
         
        
         cursor.execute(
-            "SELECT * FEROM user_data LIMIT", "OFFSET"
+            "SELECT * FROM user_data LIMIT", "OFFSET"
             
         )
         return cursor.fetchall()
