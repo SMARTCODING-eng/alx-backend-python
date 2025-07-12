f53d5a319c3b12560963e0c99f8bd94c3bea835c
Elutayo <elutayojohnson@gmail.com>
2025-07-12 13:58:51 +0100

    modify databaseconnection

--- python-context-async-operations-0x02/0-databaseconnection.py
+++ python-context-async-operations-0x02/0-databaseconnection.py
@@ -1,28 +1,25 @@
-"""
-create a class based context manager to handle opening and closing database connections automatically
-Write a class custom context manager DatabaseConnection using the __enter__ and the __exit__ methods
 
-Use the context manager with the with statement to be able to perform the query SELECT * FROM users. Print the results from the query.
-"""
+
 class DatabaseConnection:
+    def __init__(self ):
+        self.connection = None
+        self.cursor = None
+
+
     def __enter__(self):
-        self.connection = "Database connection openeed"
-        print(self.connection)
+        self.connect()
         return self
     
-    def __exit__(self):
-        print("Database connection closed")
-        self.connection = None
+    def __exit__(self, exc_type, exc_value):
+        self.close()
+        if exc_type is not None:
+            print(f"An error occurred: {exc_value}")
         return False
-    
-    def query(self, sql):
-        with sql == "SELECT * FROM users":
-            return [
-                {"id": 1, "name": "Ola mide"},
-                {"id": 2, "name": "John Doe"}
-            ]
+
+    def query(self, query):
+        with self:
+            query = "SELECT * FROM users."
+            self.cursor.execute(query)
+            return self.cursor.fetchall()
         
-with DatabaseConnection() as db:
-    results = db.query("SELECT * FROM users")
-    for row in results:
-        print(row)  
+
