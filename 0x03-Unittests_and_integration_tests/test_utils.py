4d7eb617c33225b451ffb7c2c17fd971bfcf2abd
Elutayo <elutayojohnson@gmail.com>
2025-07-18 14:24:32 +0100

    adding new file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,37 +1,41 @@
-
 #!/usr/bin/env python3
+
 import unittest
+from parameterized import parameterized
+
 
 def access_nested_map(nested_map, path):
-    """"Access a value in a nested dictionary using a sequence of key.
-    args:
+    """
+    Access a value in a nested dictionary using a sequence of keys.
+
+    Args:
         nested_map (dict): The nested dictionary to access.
-        path (tuple): A sequence of keys  representing the path to the value.
-        
-        Returns:
-        The value at the specified path in the nested dictionary.
-        """
+        path (tuple): A sequence of keys representing the path to the value.
+
+    Returns:
+        The value reached by following the path in the nested_map.
+
+    Example:
+        >>> access_nested_map({"a": 1}, ("a",))
+        1
+        >>> access_nested_map({"a": {"b": 2}}, ("a", "b"))
+        2
+    """
     current = nested_map
     for key in path:
         current = current[key]
     return current
 
+
 class TestAccessNestedMap(unittest.TestCase):
-    def test_access_nested_map_top_level(self):
-        nested_map = {"a": 1}
-        self.assertEqual(access_nested_map(nested_map, ("a",)), 1)
-
-    def test_access_nested_map_nested_dict(self):
-        nested_map = {"a": {"b": 2}}
-        self.assertEqual(access_nested_map(nested_map, ("a",)), {"b": 2})
-
-
-    def test_access_nested_map_nested_value(self):
-        nested_map = {"a": {"b": 2}}
-        self.assertEqual(access_nested_map(nested_map, ("a", "b")), 2)
-
-
-
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test that access_nested_map returns the correct value for given inputs"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 if __name__ == "__main__":
     unittest.main()
