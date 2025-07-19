2f87a746e5499c7dfcc226e3be00847e6de29972
Elutayo <elutayojohnson@gmail.com>
2025-07-19 05:00:46 +0100

    modify test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+"""Generic utilities for github org client.
+"""
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
@@ -72,6 +73,8 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
 def get_json(url: str) -> Dict:
+    """Get JSON from remote URL.
+    """
     response = requests.get(url)
     return response.json()
 
@@ -92,9 +95,25 @@ class TestGetJson(unittest.TestCase):
 
 
 def memoize(fn: Callable) -> Callable:
+    """Decorator to memoize a method.
+    Example
+    -------
+    class MyClass:
+        @memoize
+        def a_method(self):
+            print("a_method called")
+            return 42
+    >>> my_object = MyClass()
+    >>> my_object.a_method
+    a_method called
+    42
+    >>> my_object.a_method
+    42
+    """
     attr_name = "_{}".format(fn.__name__)
     @wraps(fn)
     def memoized(self):
+        """"memoized wraps"""
         if not hasattr(self, attr_name):
             setattr(self, attr_name, fn(self))
         return getattr(self, attr_name)
