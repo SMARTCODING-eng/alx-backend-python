d31bccc5450bff56eb3ce6fe45577f2afb8997f6
Elutayo <elutayojohnson@gmail.com>
2025-07-20 08:01:28 +0100

    new Django project

---
+++ messaging_app/chats/models.py
@@ -0,0 +1,86 @@
+import uuid
+from django.contrib.auth.models import AbstractBaseUser
+
+from django.db import models
+
+
+class User(AbstractBaseUser):
+    username = None
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    first_name = models.CharField(max_length=200, null=False)
+    last_name = models.CharField(max_length=200, null=False)
+    email = models.EmailField(unique=True, null=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+
+
+    class Role(models.TextChoices):
+        GUEST = 'guest', 'Guest'
+        HOST = 'host', 'Host'
+        ADMIN = 'admin', 'Admin'
+
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.GUEST,
+        null=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="user_set_custom"
+    )
+    user_permission = models.ManyToManyField(
+        'auth.permission',
+        verbose_name='user_permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="user_set_custom"
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'role']
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        indexes = [
+            models.Index(fields=['email']),
+        ]
+
+class Conversation(models.Model):
+    Conversattion_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.Conversattion_id} with {self.participants.count()} participations"
+    
+    class Meta:
+        verbose_name = 'Conversation'
+        verbose_name_plural = 'Conversations'
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)   
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.email} in Conversation {self.conversation.conversation_id} at {self.sent_at.strftime('%Y-%m-%d %H:%M')}"
+
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+        ordering = ['sent_at'] 
+        indexes = [
+            models.Index(fields=['sender']),
+            models.Index(fields=['conversation']),
+        ]
